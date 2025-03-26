673e393bc3a8ae9246fa5d0ff0a9f716212508df
Matthew Illac D. Sosa <matthewillacsosa@gmail.com>
2025-03-27 02:00:38 +0800

    as you can see, i searched through the internet to do it without using a function and i came across using a loop.  As i use def inorder to define/name the function im going to use. You can clearly see that i used lens(s) in order to count the spaces between the inserted input. and used s[counter] == " ": to count only the front spaces not the other side, and lstly i added += 1 in order to add increment and i used the return s[counter:] to return and removing the spaces and printing it without the lstrip() function.

--- print_lstrip_result_without_lstrip_func.py
+++ print_lstrip_result_without_lstrip_func.py
@@ -1,3 +1,8 @@
 #Prog01. lstrip() remove the space characters at the beginning of the string. Create a program that do the same functionality without using lstrip() function.
+def remove_space(s):
+    counter = 0
+    while counter < len(s) and s[counter] == ' ':
+        counter += 1
+    return s[counter:]
 #enter "  hello world"
 #print "hello world" without using lstrip() function
