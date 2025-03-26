393f2049f5e17fdc81acdc8ca2ccc278fa0383de
Matthew Illac D. Sosa <matthewillacsosa@gmail.com>
2025-03-27 03:05:37 +0800

    i changed some minor things and additional note: the increment adder showcases what happens if string[counter] detects that it has space, well it'll be always adding in order to reach the condition. Remeber that we used counter<len(string) that if it reaches the limit for example: it detects the next letter it will stop immdeiately and the rest is done!

--- print_lstrip_result_without_lstrip_func.py
+++ print_lstrip_result_without_lstrip_func.py
@@ -1,9 +1,9 @@
 #Prog01. lstrip() remove the space characters at the beginning of the string. Create a program that do the same functionality without using lstrip() function.
-def remove_space(s):
+def remove_space(string):
     counter = 0
-    while counter < len(s) and s[counter] == ' ':
+    while counter < len(string) and string[counter] == ' ':
         counter += 1
-    return s[counter:]
+    return string[counter:]
 #enter "  hello world"
 result = input("Enter name: ")
 print ("Result:", remove_space(result))
