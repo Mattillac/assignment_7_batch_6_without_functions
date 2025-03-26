ee822bf1ab212a41623ad708a9d1eaeeea3813e4
Matthew Illac D. Sosa <matthewillacsosa@gmail.com>
2025-03-26 23:13:19 +0800

    here is my pseudocode for program 1

---
+++ print_lstrip_result_without_lstrip_func.py
@@ -0,0 +1,3 @@
+#Prog01. lstrip() remove the space characters at the beginning of the string. Create a program that do the same functionality without using lstrip() function.
+#enter "  hello world"
+#print "hello world" without using lstrip() function
