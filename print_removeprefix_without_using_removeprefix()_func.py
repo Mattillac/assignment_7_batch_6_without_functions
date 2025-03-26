8355c24ec32b189306d8800cc3eb2be546a5ef4d
Matthew Illac D. Sosa <matthewillacsosa@gmail.com>
2025-03-27 02:56:46 +0800

    here i used def prefix_remover(string, prefix): i used an if statement and used startswith() func in order to detect if it starts with the term (prefix) so it will detect the first word which is Mr. in the "Mr. Joestar" and then it detects if function has a prefix at the start it will go back to its original form. I used len() again cause it is helpful for counting variables and i used string splicing which is the string[len(prefix):] to remove the Mr.

--- print_removeprefix_without_using_removeprefix()_func.py
+++ print_removeprefix_without_using_removeprefix()_func.py
@@ -1,3 +1,9 @@
 #Prog02. removeprefix() remove the characters at the beginning of the string that matches the function parameter. Create a program that do the same functionality without using removeprefix() function.
+def prefix_remover(string, prefix):
+    if string.startswith(prefix):
+        return string[len(prefix):]
+    return string
 #enter "Mr. Joestar"
+text = "Mr. Joestar!"
+print(prefix_remover(text, "Mr. "))
 #print "Joestar"
