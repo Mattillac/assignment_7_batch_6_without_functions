35fc7bfb194c9e50fc5280502ef471ede867fedd
Matthew Illac D. Sosa <matthewillacsosa@gmail.com>
2025-03-27 16:58:45 +0800

    this is my pseudocode for program 3

---
+++ print_lowercase()_result_without_using_lowercase().py
@@ -0,0 +1,3 @@
+#Prog03. lower() converts all characters of the string into lower case. Create a program that do the same functionality without using lower() function.
+#enter "ACCESS DENIED"
+#print "access denied" without using lower() function
