aa868e911a0b033ad8b4223819902975674d58e6
Matthew Illac D. Sosa <matthewillacsosa@gmail.com>
2025-03-27 18:28:36 +0800

    i used to def my function in order to make it easier to print as it is defined as "lower_caser" and as you can see i inserted a variable of the upper and lower casings in order for my str.maketrans to read my variables of upper and lower casings, so that my text.translate will translate the organized final form of the translated_text and returning its value.

--- print_lowercase()_result_without_using_lowercase().py
+++ print_lowercase()_result_without_using_lowercase().py
@@ -1,3 +1,9 @@
 #Prog03. lower() converts all characters of the string into lower case. Create a program that do the same functionality without using lower() function.
+def lower_caser(text):
+    uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    lowercase = "abcdefghijklmnopqrstuvwxyz"
+    translated_text = str.maketrans(uppercase, lowercase)
+    return text.translate(translated_text)
+
 #enter "ACCESS DENIED"
 #print "access denied" without using lower() function
