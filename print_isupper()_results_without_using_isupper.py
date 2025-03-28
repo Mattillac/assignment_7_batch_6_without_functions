27b450ca7271ca8af39202c8ec604a1dff779002
Matthew Illac D. Sosa <matthewillacsosa@gmail.com>
2025-03-28 18:55:14 +0800

    this is my pseudocode for program 4

---
+++ print_isupper()_results_without_using_isupper.py
@@ -0,0 +1,3 @@
+#Prog04. isupper() check if all characters of the string is on upper case. Create a program that do the same functionality without using isupper() function.
+#enter "STATIKK SHIV"
+#PRINT TRUE
