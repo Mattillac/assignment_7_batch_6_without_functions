6691a73a246859c4639ed773b882b1213a330b3b
Matthew Illac D. Sosa <matthewillacsosa@gmail.com>
2025-03-28 19:42:52 +0800

    added "THE GIVEN"

--- print_endswith()_results_without_endswith.py
+++ print_endswith()_results_without_endswith.py
@@ -1,3 +1,7 @@
 #Prog05. endswith() check if the string end part matches the function parameter. Create a program that do the same functionality without using endswith() function.
+def ends_with(string, suffix):
+    if len(string) < len(suffix):
+        return False
+    return string[-len(suffix):] == suffix
 #enter "Hello World"
 #print "True" if the string ends with "World"
