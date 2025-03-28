908d1d89db3a15685f88a3ee5db2c026952179c8
Matthew Illac D. Sosa <matthewillacsosa@gmail.com>
2025-03-28 20:38:07 +0800

    scratch that last commit it is not easy to read as it is a one liner imo i=much better if its spread out, so instead i camwe back and added the follwing if and else statements just like my last last commit afaik, so if my input doers end with the same suffix that you etnered it will print true and if not then it will print false

--- print_endswith()_results_without_endswith.py
+++ print_endswith()_results_without_endswith.py
@@ -1,7 +1,14 @@
 #Prog05. endswith() check if the string end part matches the function parameter. Create a program that do the same functionality without using endswith() function.
 def ends_with(string, suffix):
-    return string[-len(suffix):] == suffix if len(string) < len(suffix) else False
+    if len(string) < len(suffix):
+        return False
+    return string[-len(suffix):] == suffix
 
-text = input("Type: ")
+text = input("Enter text: ")
 text_secondary = input("Enter suffix: ")
+
+if ends_with(text, text_secondary):
+    print("True")
+else:
+    print("False")
 #print "True" if the string ends with "World"
