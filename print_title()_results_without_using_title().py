eb3cb2dc2b3d43b81939170c03aa4f3b088bed1b
Matthew Illac D. Sosa <matthewillacsosa@gmail.com>
2025-03-28 22:52:57 +0800

    to make this much more easier we will use import string and capwords() so thanks to the built the built in function. Thanks to geeksforgeeks for this :D

--- print_title()_results_without_using_title().py
+++ print_title()_results_without_using_title().py
@@ -1,3 +1,6 @@
 #Prog10. title() makes all first letter of each word in the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using title() function.
+import string
 #enter "yOguRt yUM"
+text = input("Enter text: ")
 #print "Yogurt Yum"
+print (string.capwords(text))
