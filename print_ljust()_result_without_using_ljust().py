3c2dabd5d7dac3c145bc3e809568c6488bc3633e
Matthew Illac D. Sosa <matthewillacsosa@gmail.com>
2025-03-28 21:34:47 +0800

    here i added the final touches as you can see i changed the variables and added input etc. and now it is functioning

--- print_ljust()_result_without_using_ljust().py
+++ print_ljust()_result_without_using_ljust().py
@@ -1,11 +1,15 @@
 #Prog06. ljust() add space characters at the end of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using ljust() function.
-def ljust_string(string, length):
-    if len(string) >= length:
+def ljust_string(string, character):
+    if len(string) >=  character:
         return string
-    return string + ' ' * (length - len(string))
+    return string + ' ' * ( character- len(string))
 
 #enter "hello"
+text = input("Enter text: ")    
+character = int(input("Enter length: "))
 #add number of spaces for ex: "9"
+result = ljust_string(text, character)
+print(f"Result: '{result}'")
 #print "hello    "
 
 
