f0334266b696f780560f7ef2664be67a659475f1
Matthew Illac D. Sosa <matthewillacsosa@gmail.com>
2025-03-28 19:11:34 +0800

    i used to define a batch again so that it\ll function properply and here you can see that i used .islower() to make sure if the letters inputted are small, thus making a statement of if and if its a lowercase it'll print False but if you typed in caps it will show the correct result

--- print_isupper()_results_without_using_isupper.py
+++ print_isupper()_results_without_using_isupper.py
@@ -1,3 +1,10 @@
 #Prog04. isupper() check if all characters of the string is on upper case. Create a program that do the same functionality without using isupper() function.
+def cap_checker(string):
+    for char_check in string:
+        if char_check.islower():
+            return False
+    return True
 #enter "STATIKK SHIV"
+text = input("Enter text: ")
+print("Result:", cap_checker(text))
 #PRINT TRUE
