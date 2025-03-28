7aa5362a8015a3a56f49ff2c707ff95993a85c2e
Matthew Illac D. Sosa <matthewillacsosa@gmail.com>
2025-03-28 20:28:35 +0800

    i modified it and turned it into a one liner so taht it will be easy to read.

--- print_endswith()_results_without_endswith.py
+++ print_endswith()_results_without_endswith.py
@@ -1,8 +1,6 @@
 #Prog05. endswith() check if the string end part matches the function parameter. Create a program that do the same functionality without using endswith() function.
 def ends_with(string, suffix):
-    if len(string) < len(suffix):
-        return False
-    return string[-len(suffix):] == suffix
+    return string[-len(suffix):] == suffix if len(string) < len(suffix) else False
 
 text = input("Type: ")
 text_secondary = input("Enter suffix: ")
