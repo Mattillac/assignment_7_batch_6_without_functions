b4689c5953d58146d4e719ffd4f113fba044a31f
Matthew Illac D. Sosa <matthewillacsosa@gmail.com>
2025-03-28 22:02:17 +0800

    As you can see i used f-strings to make it easier and i had a preset variable so that it will print easily

--- print_center()_results_without_using_center.py
+++ print_center()_results_without_using_center.py
@@ -1,5 +1,9 @@
 #Prog07. center() add space characters at the beginning and at the end of the string to print the string at the center. Create a program that do the same functionality without using center() function.
+text = input("Enter text: ")
 #example "MIDDLE"
+middle_maker = int(input("Enter length: "))
+centered_text = f"{text:^{middle_maker}}"
 #PRINT "   MIDDLE   "
+print(centered_text)
 
 
