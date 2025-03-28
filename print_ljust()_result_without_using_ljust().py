800eb50643e940f5388346da9acae40bdf48860b
Matthew Illac D. Sosa <matthewillacsosa@gmail.com>
2025-03-28 21:27:05 +0800

    as you can see i defined again and using len to count if my string is longer than the length if ever my string is longer it will be returned as it is no changes. While the line 5 it'll calculate how many spaces to addd and multiplies space to add "padding"

--- print_ljust()_result_without_using_ljust().py
+++ print_ljust()_result_without_using_ljust().py
@@ -1,4 +1,9 @@
 #Prog06. ljust() add space characters at the end of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using ljust() function.
+def ljust_string(string, length):
+    if len(string) >= length:
+        return string
+    return string + ' ' * (length - len(string))
+
 #enter "hello"
 #add number of spaces for ex: "9"
 #print "hello    "
