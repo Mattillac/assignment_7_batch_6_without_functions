de72406e0239c4d540e9ff9b23a67e1effc24a46
Matthew Illac D. Sosa <matthewillacsosa@gmail.com>
2025-03-28 22:25:56 +0800

    i've created this str.maketrans so that this table of A-Z is converted to a-z and vice versa as my previous code on lower() this one has a massisve arsenal of letters so that it is fully functional but at the cost of too long and many characters, so now that my input text will be tranlsated and converted into the swapcase() like function

--- print_swapcase()_results_without_using_swapcase.py
+++ print_swapcase()_results_without_using_swapcase.py
@@ -1,3 +1,10 @@
 #Prog08. swapcase() reverse the casing of each of the character of the string. Create a program that do the same functionality without using swapcase() function.
+def bootleg_swapcase(string):
+    case_changer = str.maketrans(
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    return string.translate(case_changer)
 #enter "Hello World"
+text = input("Enter text: ")
+print(bootleg_swapcase(text))
 #print "hELLO wORLD"
